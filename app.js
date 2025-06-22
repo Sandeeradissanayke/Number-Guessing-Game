8a589054a8e9a8af986376fb7b375130ca10f3e6
Sandeera Dissanayake <Sandeeradissanayke@gmail.comgit config --global user.name Sandeeradissanayke>
2025-06-22 23:54:16 +0530

    commit

---
+++ app.js
@@ -0,0 +1,54 @@
+// /*console.log("Hello");
+
+// let x=10;
+// let y=20;
+// let z=x+y;
+
+
+// console.log(typeof z);*/
+
+// let student = {
+//     name:"Saman",
+//     age:20,
+//     address:"Panadura", 
+// };
+
+
+// let studentlist=[
+//     {
+//     name:"Saman",
+//     age:20,
+//     address:"wattala",
+//     },
+//     {
+//     name:"kasun",
+//     age:20,
+//     address:"Panadura",
+//     },
+//     {
+//     name:"nimal",
+//     age:20,
+//     address:"colombo",
+//     },
+//     {
+//     name:"Samantha",
+//     age:20,
+//     address:"kaluthara",
+//     }
+// ]
+
+// console.log(studentlist);
+// console.log(studentlist.length);
+// console.log(typeof studentlist);
+
+
+function clickAction(){
+    let text=document.getElementById("inputText")
+    let lbl = document.getElementById("label")
+    lbl.innerText=text.value;
+    console.log(text.value);
+    console.log(Math.floor(Math.random()*5));
+    
+    
+}
+
