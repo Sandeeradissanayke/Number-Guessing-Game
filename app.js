c1c44c336f45311849f9765ff948010432304c71
Sandeera Dissanayake <Sandeeradissanayke@gmail.comgit config --global user.name Sandeeradissanayke>
2025-06-23 01:13:52 +0530

    commit2

--- app.js
+++ app.js
@@ -41,14 +41,42 @@
 // console.log(studentlist.length);
 // console.log(typeof studentlist);
 
+let randomnum=(Math.floor(Math.random()*5));
+
 
 function clickAction(){
-    let text=document.getElementById("inputText")
-    let lbl = document.getElementById("label")
-    lbl.innerText=text.value;
-    console.log(text.value);
-    console.log(Math.floor(Math.random()*5));
+    console.log("Click");
     
+    let lbl1 = document.getElementById("label1")
+    let text=document.getElementById("inputText").value;
+    //debugger;
+
+    //Math.floor(Math.random()*5==text.value;
+
+    if(randomnum==text){
+        document.getElementById("imgBox").innerHTML= '<img width="300" src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExb2k1dXR1dXllZXpkc2o4dnllc2s5emRyNmtyeDdvNjAwZmQ0MXYyeSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/etKSrsbbKbqwW6vzOg/giphy.gif" alt="">'        
+        alert("you won the game!!!")
+        //console.log("you won the game!!!");
+        lbl1.innerText="you won the game!!"
+    }
+    else if(randomnum>text){
+
+        alert("your number is low !!!")
+        lbl1.innerText="you loss the game!!"
+        
+    }
+
+    else if(randomnum<text){
+
+        alert("your number is High !!!")
+        lbl1.innerText="you loss the game!!"
+        
+    }
+   
     
 }
+ //console.log(text.value);
+    
+    console.log(randomnum);
+   
 
